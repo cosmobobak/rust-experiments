4411a0221bc361d7ceaba63ab82be8b4b2ce0e20
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2021-02-02 14:22:39 +0000

    some fixes, still broken

--- TTT.rs
+++ TTT.rs
@@ -1,38 +1,41 @@
 use std::io;
-use rand::prelude::*;
+use std::io::*;
+use std::convert::TryInto;
+// use rand::prelude::*;
 
 fn lsb(bb: i16) -> i8 {
-   return ( (bb & -bb) - 1 ).count_ones();
+   return ( (bb & -bb) - 1 ).count_ones().try_into().unwrap();
 }
 
 pub struct State {
     position: [i16; 2], // = [0, 0];
-    turn: i8, // = 1;
+    turn: i16, // = 1;
     movestack: Vec<i8>,
 }
 
 impl State {
-    pub fn play(&mut self, i: i8) {
+    fn play(&mut self, i: i8) {
         // n ^ (1 << k) is a binary XOR where you flip the kth bit of n
-        if (self.turn == 1){
-            self.position[0] |= (1 << i);}
-        else{
-            self.position[1] |= (1 << i);}
+        if self.turn == 1 {
+            self.position[0] |= (1 << i);
+        } else {
+            self.position[1] |= (1 << i);
+        }
         self.turn = -self.turn;
         self.movestack.push(i);
     }
 
     // do not unplay on root
-    pub fn unplay(&mut self) {
-        let prevmove: i8 = self.movestack.pop();
-        if (self.turn == 1){
-            self.position[1] &= !(1 << prevmove);}
-        else{
+    fn unplay(&mut self) {
+        let prevmove: i8 = self.movestack.pop().unwrap();
+        if self.turn == 1 {
+            self.position[1] &= !(1 << prevmove);
+        } else {
             self.position[0] &= !(1 << prevmove);}
         self.turn = -self.turn;
     }
 
-    pub fn pos_filled(&self, i: i8) -> bool {
+    fn pos_filled(&self, i: i8) -> bool {
         if (((self.position[0] | self.position[1]) & (1 << i)) != 0) {
             return true;
         } else {
@@ -41,7 +44,7 @@ impl State {
     }
 
     // only valid to use if pos_filled() returns true, true = x, false = y
-    pub fn player_at(&self, i: i8) -> bool {
+    fn player_at(&self, i: i8) -> bool {
         if ((self.position[0] & (1 << i)) != 0) {
             return true;
         } else {
@@ -49,7 +52,7 @@ impl State {
         }
     }
 
-    pub fn is_full(&self) -> bool {
+    fn is_full(&self) -> bool {
         for i in 0..9 {
             if (!self.pos_filled(i)){
                 return false;}
@@ -57,7 +60,7 @@ impl State {
         return true;
     }
 
-    pub fn evaluate(&self) -> i8 {
+    fn evaluate(&self) -> i16 {
         // check first diagonal
         if (self.pos_filled(0) && self.pos_filled(4) && self.pos_filled(8)) {
             if (self.player_at(0) == self.player_at(4) && self.player_at(4) == self.player_at(8)) {
@@ -101,11 +104,11 @@ impl State {
         return 0;
     }
 
-    pub fn pass_turn(&self) {
+    fn pass_turn(&mut self) {
         self.turn = -self.turn;
     }
 
-    pub fn show(&self) {
+    fn show(&self) {
         for x in 0..3 {
             for y in 0..3 {
                 if (self.pos_filled(x * 3 + y)) {
@@ -121,31 +124,31 @@ impl State {
         println!("\n");
     }
 
-    pub fn is_game_over(&self) -> bool {
+    fn is_game_over(&self) -> bool {
         return (self.evaluate() != 0) || self.is_full();
     }
 
-    pub fn num_legal_moves(&self) {
+    fn num_legal_moves(&self) -> u32 {
         return 9 - (self.position[0] | self.position[1]).count_ones();
     }
 
-    pub fn legal_moves(&self) -> Vec<i8> {
-        let moves: Vec::<i8>;
+    fn legal_moves(&self) -> Vec<i8> {
+        let mut moves: Vec::<i8> = Vec::new();
         moves.reserve(9);
-        let bb: i16 = !(self.position[0] | self.position[1]) & 0b111111111;
-        while bb {
-            moves.push_back(lsb(bb));
+        let mut bb: i16 = !(self.position[0] | self.position[1]) & 0b111111111;
+        while bb != 0 {
+            moves.push(lsb(bb));
             bb &= bb - 1;  // clear the least significant bit set
         }
         return moves;
     }
 
-    pub fn random_play(&mut self) {
-        let moves: Vec::<i8> = self.legal_moves();
-        self.play(moves[rand::random::<i32>() % moves.size()]);
-    }
+    // pub fn random_play(&mut self) {
+    //     let moves: Vec::<i8> = self.legal_moves();
+    //     self.play(moves[rand::random::<i32>() % moves.size()]);
+    // }
 
-    pub fn heuristic_value(&self) -> i8 {
+    fn heuristic_value(&self) -> i8 {
         return 0;
     }
 }
@@ -159,15 +162,15 @@ pub struct Istus {
 impl Istus {
     fn negamax(
         &mut self,
-        colour: i8, 
-        a: i16, 
+        colour: i16, 
+        mut a: i16, 
         b: i16) -> i16  //WORKING
     {
         if (self.node.is_game_over()) {
             self.nodes += 1;
             return colour * self.node.evaluate();
         }
-        let score: i16;
+        let mut score: i16;
 
         // node.pass_turn();                              // MAKE A NULL MOVE
         // score = -negamax(depth - 3, -colour, -b, -a);  // PERFORM A LIMITED SEARCH
@@ -193,11 +196,11 @@ impl Istus {
         return a;
     }
 
-    pub fn engine_move(&mut self)  //WORKING
+    fn engine_move(&mut self)  //WORKING
     {
-        let bestmove: i8;
-        let bestcase: i8 = -2;
-        let score: i8;
+        let mut bestmove: i8 = 0;
+        let mut bestcase: i16 = -2;
+        let mut score: i16 = 0;
         self.reset_nodes();
         for m in self.node.legal_moves() {
             self.node.play(m);
@@ -209,16 +212,16 @@ impl Istus {
             }
         }
         println!("ISTUS:\n");
-        println!("{} nodes processed.\n", nodes);
+        println!("{} nodes processed.\n", self.nodes);
         println!("Istus win prediction: {}%\n", ((1 + bestcase) * (50)));
-        node.play(bestmove);
+        self.node.play(bestmove);
     }
 
-    pub fn reset_nodes(&mut self) {
+    fn reset_nodes(&mut self) {
         self.nodes = 0;
     }
 
-    pub fn show_result(&self) {
+    fn show_result(&self) {
         let r = self.node.evaluate();
         if (r == 0){
             println!("1/2-1/2\n");}
@@ -228,20 +231,32 @@ impl Istus {
             println!("0-1\n");}
     }
 
-    pub fn get_player_move() -> i8 {
+    fn get_player_move(&self) -> i8 {
         let legals: Vec::<i8> = self.node.legal_moves();
-        pos: i8;
-        io::stdin()
-            .read_line(&mut pos)
-            .expect("Failed to read line");
-        while (legals.all(|&x| x != pos + 1)) {
-            node.show();
-            std::cin >> pos;
+        let mut pos: i8;
+        let mut strpos = String::new();
+        std::io::stdin().read_line(&mut strpos).expect("0");
+        pos = str::parse::<i8>(&strpos).unwrap();
+        while (legals.iter().all(|&x| x != pos + 1)) {
+            self.node.show();
+            
+            std::io::stdin().read_line(&mut strpos).expect("0");
+            pos = str::parse::<i8>(&strpos).unwrap();
         }
         return pos - 1;
     }
 }
-
 fn main(){
-    println!("Hello World!");
+    let mut game = Istus { node: State { position: [0, 0], turn: 1, movestack: [].to_vec() }, nodes: 0,
+    timeLimit: 1000 };
+    let mut m: i8;
+    game.node.show();
+    while !game.node.is_game_over() {
+        m = game.get_player_move();
+        game.node.play(m);
+        game.node.show();
+        game.engine_move();
+        game.node.show();
+    }
+    game.show_result();
 }
